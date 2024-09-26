3ed49019d7c8c37cdb7ebf785f3dd66bc1babe6b
kczulko <karol.czulkowski@protonmail.com>
2024-09-26 22:01:03 +0200

    feat: bazel build for gui and tui targets

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Dev env for lagrange compilation";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { ... } @ args: with args;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+
+        shells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bazel_6
+              nix
+              gdb
+            ];
+          };
+        };
+      in
+      {
+        devShells = shells;
+        devShell = shells.default;
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
